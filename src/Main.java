390152b2cd280b509c9fde108d73b6f0ceb20f84
EMRE ŞAHBUDAK <118505939+EmreSahbudak@users.noreply.github.com>
2023-05-21 12:59:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+            for (int i = 0; i <= 10; i += 2) {
+                if(i==0){
+                    System.out.print("*");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            System.out.println("\nEmre Sahbudak 4.Soru cozumu (19.05.2023)");
+    }
+}
